d36d8c99a36890f1d38ff4b0985b1bfae7fa8ada
PartySlayer <squadrittichristian@gmail.com>
2025-10-24 22:36:47 +0200

    Logica subnets: creata mappa che associa CIDR in base al tipo di subnet; convertita in lista per ciclo for_each.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # terraform-workspaces
+
 Didattica Terraform. Obiettivi dimostrativi: creare moduli riutilizzabili. gestire ambienti multipli con workspaces e Best Practice
+
+## Fase 1: infrastruttura iniziale
+
+- (WIP) Migliorare [il vecchio modulo VPC scritto 2 anni fa](https://github.com/PartySlayer/tf-vpc) come punto di partenza.
+- Creare un modulo per un cluster EKS che utilizzi il modulo VPC come dipendenza.
+- Creare un modulo per un'istanza RDS (PostgreSQL) modulare e configurabile.
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -26,3 +26,30 @@ resource "aws_internet_gateway" "main_ig" {
   })
 }
 
+# LOGICA PER LE SUBNET 
+
+
+# Mappa per associare il tipo di subnet alla lista di CIDR
+
+locals {
+  subnet_cidrs = {
+    public  = var.public_subnet_cidrs
+    private = var.private_app_subnet_cidrs
+    data    = var.private_data_subnet_cidrs
+  }
+
+  # Lista piatta da mappa subnets_cidrs
+  # Il nome univoco ("public_0" [..]) sarà la primary key
+
+  subnets = flatten([
+    for type, cidrs in local.subnet_cidrs : [
+      for i, cidr in cidrs : {
+        key          = "${type}_${i}"
+        type         = type
+        cidr         = cidr
+        az           = data.aws_availability_zones.available.names[i]
+      }
+    ]
+  ])
+}
+
